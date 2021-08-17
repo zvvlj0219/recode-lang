cd492f830729de73bcc90bfb6ffc85f11d88c5f6
zvvlj0219 <s00302448@gmail.com>
2021-08-17 14:46:05 +0900

    setting API route

--- server/routes/api/v1/timer.js
+++ server/routes/api/v1/timer.js
@@ -2,9 +2,54 @@ const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
 
-//sample
-router.get('/',(req,res)=>{
-  res.send('timer')
-})
+// mongoose connect to mongoDB Atlas
+const uri = "mongodb+srv://zvvlj0219:se9108li07da13@cluster0.g2zyh.mongodb.net/test2?retryWrites=true&w=majority";
+
+const options = {
+	useUnifiedTopology : true,
+	useNewUrlParser : true
+}
+mongoose.connect(uri,options);
+
+//mongoose schema
+const timerSchema = new mongoose.Schema(
+  {
+    language:String,
+    study_time:Number,
+    timestamps:Object
+  },
+  {
+    collection:'timer'
+  }
+);
+
+//mongoose model
+const Timer = mongoose.model('Timer',timerSchema);
+
+//get posts
+router.get('/',async (req,res)=>{
+  try{
+    const time = await Timer.find().limit(5);
+    res.send(time);
+  }catch(e){
+    res.status(500).send();
+  }
+});
+
+//add posts
+router.post('/',async (req,res)=>{
+  try{
+    const data = {
+      language:req.body.language,
+      study_time:req.body.study_time,
+      timestamps:new Date()
+    }
+    const newTimer = await Timer.create(data);
+    await newTimer.save();
+    res.status(201).send(newTimer);
+  }catch(e){
+    res.status(400).send();
+  }
+});
 
 module.exports = router;
--- server/routes/api/v1/todo.js
+++ server/routes/api/v1/todo.js
@@ -2,9 +2,92 @@ const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
 
-//sample
-router.get('/',(req,res)=>{
-  res.send('todo')
+// mongoose connect to mongoDB Atlas
+const uri = "mongodb+srv://zvvlj0219:se9108li07da13@cluster0.g2zyh.mongodb.net/test2?retryWrites=true&w=majority";
+
+const options = {
+	useUnifiedTopology : true,
+	useNewUrlParser : true
+}
+mongoose.connect(uri,options);
+
+//mongoose schema
+const todoSchema = new mongoose.Schema(
+  {
+    language:String,
+    text:String,
+    study_time:Number,
+    timestamps:Object
+  },
+  {
+    collection:'todo'
+  }
+)
+
+//mongoose model
+const Todo = mongoose.model('Todo',todoSchema);
+
+//get posts
+router.get('/',async (req,res)=>{
+  try{
+    const todo = await Todo.find({});
+    res.send(todo)
+    console.log(todo)
+    
+  }catch(e){
+    res.status(500).send()
+  }
+});
+
+
+//add posts
+router.post('/',async (req,res)=>{
+  try{
+    const data = {
+      language:req.body.language,
+      text:req.body.text,
+      study_time:1,    
+      timestamps:new Date()
+    }
+    const newTodo = await Todo.create(data);
+    await newTodo.save();
+    res.status(201).send(newTodo);
+    console.log(newTodo)
+  }catch(e){
+    res.status(400).send();
+  }
+});
+
+//update
+router.put('/:id',async (req,res)=>{
+  try{
+    const todo = await Todo.findByIdAndUpdate(
+      //id
+      req.params.id,
+      //update
+      {text:req.body.text}
+    );
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send(todo);
+  }catch(e){
+    res.status(500).send();
+  }
+
+})
+
+//delete posts
+router.delete('/:id',async (req,res)=>{
+  try{
+    const todo = await Todo.findByIdAndDelete(req.params.id);
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send(todo);
+  }catch(e){
+    res.status(500).send();
+  }
 })
 
 module.exports = router;
