fb04afcad393b993040c0fbc64ec2d09628a6e22
zvvlj0219 <s00302448@gmail.com>
2021-08-17 12:36:20 +0900

    create vue component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
@@ -11,7 +10,6 @@ import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
---
+++ client/src/components/dashboard.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>this is dashboard</div>
+</template>
