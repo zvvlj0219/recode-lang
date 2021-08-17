b7ad189a2efe0e780ab2cce96e9ef61a12c75660
zvvlj0219 <s00302448@gmail.com>
2021-08-17 12:27:04 +0900

    create index.js && prep server

---
+++ server/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+// middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+//API routes
+// const todo = require('./routes/api/v1/todo');
+// const timer = require('./routes/api/v1/timer');
+// app.use('/api/v1/todo',todo);
+// app.use('/api/v1/timer',timer);
+
+//sample
+app.get('/',(req,res)=>{
+  res.send('hello')
+})
+
+//handle production
+if(process.env.NODE_ENV === 'produnction'){
+  //static folder
+  app.use(express.static(__dirname + '/public'));
+  
+  //handle spa
+  app.get(/.*/,(req,res)=>res.sendFile(__dirname + 'public/index.html'));
+}
+
+// server 
+const port = process.env.PORT || 5000;
+app.listen(port,()=>{
+  console.log('server is active port 5000')
+})
