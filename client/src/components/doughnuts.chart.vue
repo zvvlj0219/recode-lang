6fef4a4ff5df974ef36a7a39b698a8820ae74290
zvvlj0219 <s00302448@gmail.com>
2021-09-29 16:24:03 +0900

    created dashboard api

--- client/src/components/doughnuts.chart.vue
+++ client/src/components/doughnuts.chart.vue
@@ -4,62 +4,64 @@ import _ from 'lodash';
 import all_langs from '../modules/languages.js';
 export default {
   extends:Doughnut,
-  data(){
-    return {
-      mongo:[
-        {language:'JavaScript',study_time:1},
-        {language:'React.js',study_time:2},
-        {language:'Node.js',study_time:2},
-      ],
-      //1親コンポーネントからmongoDB から今日の分のデータを抽出しpropsで渡す、言語、時間
-      //{language:'JavaScript',study_time:2}の形式で
+  props:{
+    today:{
+      type:Array,
+      required:true,
+    },
+  },
+  watch:{
+    today(){
+      this.render();
     }
   },
-  props:{
-    study_data:Object
-  },
-  mounted(){
-    const labels = this.mongo.map(el=>{
-      return el.language;
-    })
+  methods:{
+    render(){
+      console.log('today records')
+      console.log(this.today)
+      const labels = this.today.map(el=>{
+        return el.language;
+      })
 
-    const lodash_object = all_langs.filter(element=>{
-      return _.findIndex(this.mongo, {'language': element.language}) !== -1 ;
-    })
-    const backgroundColor =  lodash_object.map(el=>{
-      return el.color;
-    });
-    
-    const study_time = this.mongo.map(el=>{
-      return el.study_time;
-    })
+      const lodash_object = all_langs.filter(element=>{
+        return _.findIndex(this.today, {'language': element.language}) !== -1 ;
+      })
+      const backgroundColor = lodash_object.map(el=>{
+        return el.color;
+      });
+      
+      const study_time = this.today.map(el=>{
+        return el.study_time;
+      })
 
-    const data = {
-      labels:labels,
-      datasets: [{
-        data: study_time,
-        backgroundColor:backgroundColor,
-        borderWidth:2,
-        borderColor:'lightgrey',
-        weight:1,
-        hoverOffset: 4
-      }]
-    };
+      const data = {
+        labels:labels,
+        datasets: [{
+          data: study_time,
+          backgroundColor:backgroundColor,
+          borderWidth:2,
+          borderColor:'lightgrey',
+          weight:1,
+          hoverOffset: 4
+        }]
+      };
 
-    const options = {
-      responsive: false,
-      cutoutPercentage: 70,
-      legend: {
-        position: 'bottom'
-      },
-      title: {
-        display: true,
-        text: "Today's record",
-        fontSize: 18,
-        position: 'top'
-      },
-    };
-    this.renderChart(data,options)
+      const options = {
+        responsive: false,
+        cutoutPercentage: 70,
+        legend: {
+          position: 'bottom'
+        },
+        title: {
+          display: true,
+          text: "Today's record",
+          fontSize: 18,
+          position: 'top'
+        },
+      };
+      this.renderChart(data,options)
+
+    }
   }
 }
 </script>
--- server/routes/api/v1/dashboard.js
+++ server/routes/api/v1/dashboard.js
@@ -2,9 +2,66 @@ const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
 
-//sample
-router.get('/',(req,res)=>{
-  res.send('dshboard')
-})
+//tentative account email
+const email = 'sample1234@mouse.com';
+
+// mongoose connect to mongoDB Atlas
+const uri = require('../../../config');
+
+const options = {
+	useUnifiedTopology : true,
+	useNewUrlParser : true,
+  useFindAndModify : false
+}
+mongoose.connect(uri,options);
+
+//mongoose schema
+const dashboardSchema = new mongoose.Schema(
+  {
+    email:String,
+    language:String,
+    created_at:String,
+    study_time:Number,
+    timestamps:Object
+  },
+  {
+    collection:'record'
+  }
+);
+
+//mongoose model
+const Dashboard = mongoose.model('Dashboard',dashboardSchema);
+
+//zeroday sixday
+let today = new Date();
+let day = today.getDay();
+let zeroday = new Date();
+let sixday = new Date();
+zeroday.setFullYear(today.getFullYear());
+zeroday.setMonth(today.getMonth());
+zeroday.setDate(today.getDate()-day);
+zeroday.setHours(0);
+zeroday.setMinutes(0);
+zeroday.setSeconds(0);
+sixday.setDate(zeroday.getDate()+6);
+
+//get dashboard
+router.get('/',async (req,res)=>{
+  try{
+    const result = await Dashboard.find(
+      {
+        email:email,
+        timestamps:{
+          $gte:zeroday,
+          $lte:sixday
+        }
+      }
+    )
+    .select(['language','study_time','timestamps']);
+    res.send(result);
+  }catch(e){
+    res.status(500).send();
+  }
+});
 
 module.exports = router;
